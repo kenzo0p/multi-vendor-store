cbe60c1d9526dc0990c7f28d0efcdfb014d2e331
Om Bhor <ombhor617@gmail.com>
2025-04-19 14:36:46 +0530

    Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="text-red-500">
+      <p className="font-bold">Hello world</p>
+    </div>
+  );
+}
