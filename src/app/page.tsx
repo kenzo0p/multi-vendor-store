b3c6dd6400b17ced619a7acea60dc368232ebc90
Om Bhor <ombhor617@gmail.com>
2025-04-19 15:04:09 +0530

    Classes customization for neuobrutalnims theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-red-500">
-      <p className="font-bold">Hello world</p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">I am Button</Button>
+        </div>
+        <div>
+          <Input placeholder="I am input"></Input>
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea value="I am a textarea" />
+        </div>
+        <div>
+          <Checkbox></Checkbox>
+        </div>
+      </div>
     </div>
   );
 }
