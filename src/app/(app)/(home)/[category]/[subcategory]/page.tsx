3fbe25f743c964476969c28efe5b432d1af36a2a
Om Bhor <ombhor617@gmail.com>
2025-05-03 16:09:50 +0530

    Add product and category management features
    - Introduced Products collection with fields for name, description, price, category, image, and refund policy.
    - Updated Categories collection to use 'name' as the title in the admin interface.
    - Implemented TRPC router for products with a query to fetch products by category.
    - Enhanced page components to include product listing and skeleton loading states.
    - Updated environment configuration and ignored .env files for security.

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,14 +1,25 @@
+import  { ProductList,
+  ProductListSkeleton,
+} from "@/modules/products/ui/components/product-list";
+import { trpc, getQueryClient } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
 interface Props {
-    params : Promise<{category : string , subcategory : string}>
+  params: Promise<{ subCategory: string }>;
 }
 
-const page = async({params} : Props) => {
-  const {category , subcategory} = await params;
+const page = async ({params}: Props) => {
+  const {subCategory} = await params;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({category:subCategory}));
   return (
-    <div>
-      category : {category}
-      subCategory :  {subcategory}</div>
-  )
-}
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<ProductListSkeleton />}>
+        <ProductList category={subCategory} />
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
 
-export default page
+export default page;
